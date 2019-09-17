7ad5071ca22c88fa934f5d3c2b3e847839d4f0df
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2019-09-17 16:37:57 -0600

    17 Sept 2019: finally adding all_lowercase_names.py

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # IF-utils
 Small collection of utilities related to interactive fiction.
 
+## `all_lowercase_names.py`
+Converts all filename extensions in a directory to lowercase, after first checking that doing so will not clobber any existing files. This is occasionally useful for Linux users trying to play AGT games in `agility`.
+
+Usage: `all_lowercase_names.py [dirname]`. If `dirname` is not specified, operates on the current working directory.
+
 ## `ap2zc.py`
-Extracts the Infocom story files (usually .z3 or .z5) from raw (256-byte sector) Apple II disk images. Usage: `ap2zc.py INPUT_FILE_NAME`
+Extracts the Infocom story files (usually .z3 or .z5) from raw (256-byte sector) Apple II disk images.
+
+Usage: `ap2zc.py INPUT_FILE_NAME`
 
 ## `transcript_utils/visual_transcript/`
 A series of scripts intended to help process postprocess screencast videos of textual IF when standard transcripting is not possible. Run in numerical order to create a series of frames as `.png` files that have had duplicate frames largely removed from short sequences.
---
+++ all_lowercase_names.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""A quick hack to rename all files in the current directory so that the filename
+extensions are all lowercase. agility expects this, and under Linux, filename
+case does matter. When Agility was originally written, AGT games were typically
+extracted from .zip archives on the command line under Linux, and lowercase
+filename extensions were easy to ensure with the -L option to `unzip`; but this
+is less easy to ensure in a graphically driven Linux, so this utility just
+lowercases all filename extensions in the directory, first checking to make
+sure that this won't clobber any existing files.
+
+Usage:
+    all_lowercase_names.py [dirname]
+
+If DIRNAME is not specified, the current working directory is used.
+
+Alternately, -h or --help may be specified to print this message.
+"""
+
+
+import os, shlex, sys
+from pathlib import Path
+
+
+def transform_case(p: Path) -> Path:
+    """Returns a modified version of path P in which the filename's suffix is entirely
+    in lowercase."""
+    suffix = os.path.splitext(p)[1].lower()
+    return p.with_suffix(suffix)
+
+
+if len(sys.argv) > 1:
+    if sys.argv[1].lower() in ['--help', '-h']:
+        print(__doc__)
+        sys.exit(0)
+    elif len(sys.argv) > 2:
+        print(__doc__)
+        sys.exit(1)
+    else:
+        working_dir = Path(sys.argv[1])
+else:
+    working_dir = Path(os.getcwd())
+
+for f in working_dir.glob("*"):
+    lower_f = transform_case(f)
+    if not lower_f.samefile(f):
+        assert not lower_f.exists(), "ERROR! Lowercasing all names would overwrite file %s!" % shlex.quote(str(f))
+
+print("About to process all files in directory %s ..." % shlex.quote(f))
+
+for f in working_dir.glob("*"):
+    lower_f = transform_case(f)
+    if not f.samefile(lower_f):
+        f.rename(lower_f)
