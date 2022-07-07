ff314cea3b00dc420dcde49e863e3b53d2aabeb9
Patrick Mooney <github.pmooney78@neverbox.com>
2022-07-07 00:35:01 -0500

    6 Jul 2022: minor structure update to ATD-exploring code

--- specific_games/ATD/setup.py
+++ specific_games/ATD/setup.py
@@ -4,5 +4,5 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize('explore_possibilities.pyx'))
+setup(ext_modules=cythonize('explore_possibilities.py'))
 
